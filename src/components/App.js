fb6f2dfa4f29685757efb8a9a499697516cb6705
haney2121 <haney037332@gmail.com>
2019-05-22 23:37:14 -0500

    added HOC for language context and darkmode

---
+++ src/components/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import PageContent from './PageContent';
+import Navbar from './Navbar';
+import Form from './Form';
+import { ThemeProvider } from '../contexts/ThemeContext';
+import { LanguageProvider } from '../contexts/LanguageContext';
+
+const App = () => {
+  return (
+    <ThemeProvider>
+      <LanguageProvider>
+        <PageContent>
+          <Navbar />
+          <Form />
+        </PageContent>
+      </LanguageProvider>
+    </ThemeProvider >
+  );
+}
+
+export default App;
---
+++ src/components/Form.js
@@ -0,0 +1,79 @@
+import React, { Component } from 'react';
+
+import { ThemeContext } from '../contexts/ThemeContext';
+import { withLanguageContext } from '../contexts/LanguageContext';
+
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import FormControl from '@material-ui/core/FormControl';
+import Checkbox from '@material-ui/core/Checkbox';
+import Input from '@material-ui/core/Input';
+import InputLabel from '@material-ui/core/InputLabel';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+import MenuItem from '@material-ui/core/MenuItem';
+import Select from '@material-ui/core/Select';
+import { withStyles } from '@material-ui/core/styles';
+import styles from '../styles/FormStyles';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+
+const words = {
+  english: {
+    email: 'Email',
+    signIn: 'Sign In',
+    password: 'Password',
+    remember: 'Remember Me'
+  },
+  french: {
+    email: 'Adresse électronique',
+    signIn: 'Se connecter',
+    password: 'Mot de passe',
+    remember: 'Souviens-toi de moi'
+  },
+  german: {
+    email: 'E-Mail-Addresse',
+    signIn: 'Einloggen',
+    password: 'Passwort',
+    remember: 'Erinnere dich an mich'
+  }
+}
+
+class Form extends Component {
+  static contextType = ThemeContext;
+  render() {
+    const { isDarkMode } = this.context;
+    const { language, changeLanguage } = this.props.languageContext;
+    const { classes } = this.props;
+    const { email, signIn, password, remember } = words[language];
+    return (
+      <main className={classes.main}>
+        <Paper className={classes.paper}>
+          <Avatar className={classes.avatar}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography variant='h5'>{signIn}</Typography>
+          <Select value={language} onChange={changeLanguage}>
+            <MenuItem value='english'>English</MenuItem>
+            <MenuItem value='french'>French</MenuItem>
+            <MenuItem value='german'>German</MenuItem>
+          </Select>
+          <form className={classes.form}>
+            <FormControl margin='normal' require='true' fullWidth>
+              <InputLabel htmlFor='email'>{email}</InputLabel>
+              <Input id='email' name='email' autoFocus />
+            </FormControl>
+            <FormControl margin='normal' require='true' fullWidth>
+              <InputLabel htmlFor='password'>{password}</InputLabel>
+              <Input id='password' name='password' />
+            </FormControl>
+            <FormControlLabel control={<Checkbox color='primary' />} label={remember} />
+            <Button variant='contained' type='submit' fullWidth color={isDarkMode ? 'default' : 'primary'} className={classes.submit}>{signIn}</Button>
+          </form>
+        </Paper>
+      </main>
+    )
+  }
+}
+
+export default withLanguageContext(withStyles(styles)(Form));
